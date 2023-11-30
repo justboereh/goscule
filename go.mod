e0ba2ccaa6431b952251b18297f174520910be46
Boe Reh <62665965+justboereh@users.noreply.github.com>
2023-11-30 04:49:25 +0000

    first

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/justboereh/goscule
+
+go 1.21.3
---
+++ goscule.go
@@ -0,0 +1,29 @@
+package goscule
+
+func splitByCase(str string, sep string) {
+
+}
+
+func Pascal(str string) string {
+
+}
+
+func Camel(str string) string {
+
+}
+
+func Kebab(str string) string {
+
+}
+
+func Snake(str string) string {
+
+}
+
+func Upper(str string) string {
+
+}
+
+func Lower(str string) string {
+
+}
